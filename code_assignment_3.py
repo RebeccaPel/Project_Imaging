affa869ea1754a81078c01da559fb8d9093799e1
RebeccaPel <r.n.pelsser@student.tue.nl>
2022-02-16 17:02:16 +0100

    Added 5 lines to make the ROC curve

--- code_assignment_3.py
+++ code_assignment_3.py
@@ -20,6 +20,7 @@ from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 
 # unused for now, to be used for ROC analysis
 from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 
 # the size of the images in the PCAM dataset
 IMAGE_SIZE = 96
@@ -28,8 +29,8 @@ IMAGE_SIZE = 96
 def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
 
      # dataset parameters
-     train_path = os.path.join(base_dir, 'train')#'train+val', 'train')
-     valid_path = os.path.join(base_dir, 'valid')#'train+val', 'valid')
+     train_path = os.path.join(base_dir, 'train')
+     valid_path = os.path.join(base_dir, 'valid')
 
 
      RESCALING_FACTOR = 1./255
@@ -41,14 +42,14 @@ def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
                                              target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                              batch_size=train_batch_size,
                                              class_mode='binary')
-     train_gen_keys = train_gen.class_indices.keys()
+     #train_gen_keys = train_gen.class_indices.keys()
      val_gen = datagen.flow_from_directory(valid_path,
                                              target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                              batch_size=val_batch_size,
                                              class_mode='binary')
-     val_gen_keys = val_gen.class_indices.keys()
+     #val_gen_keys = val_gen.class_indices.keys()
      
-     return train_gen, val_gen, train_gen_keys, val_gen_keys
+     return train_gen, val_gen, datagen
 
 
 def get_model(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filters=64):
@@ -66,7 +67,6 @@ def get_model(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filte
      model.add(Dense(64, activation = 'relu'))
      model.add(Dense(1, activation = 'sigmoid'))
 
-
      # compile the model
      model.compile(SGD(learning_rate=0.01, momentum=0.95), loss = 'binary_crossentropy', metrics=['accuracy'])
 
@@ -78,7 +78,7 @@ model = get_model()
 
 
 # get the data generators
-train_gen, val_gen, train_gen_keys, val_gen_keys = get_pcam_generators(r'C:\Users\20192157\OneDrive - TU Eindhoven\Documents\Uni\J3-Q3\8P361 Project Imaging')
+train_gen, val_gen, datagen = get_pcam_generators(r'C:\Users\20192157\OneDrive - TU Eindhoven\Documents\Uni\J3-Q3\8P361 Project Imaging')
 
 
 
@@ -102,86 +102,112 @@ callbacks_list = [checkpoint, tensorboard]
 train_steps = train_gen.n//train_gen.batch_size
 val_steps = val_gen.n//val_gen.batch_size
 
-history = model.fit(train_gen, steps_per_epoch=train_steps,
+history = model.fit(train_gen, steps_per_epoch=train_steps/20,
                     validation_data=val_gen,
-                    validation_steps=val_steps,
-                    epochs=3,
+                    validation_steps=val_steps/20,
+                    epochs=1,
                     callbacks=callbacks_list)
 
-score = history.evaluate(val_gen, val_gen_keys, verbose=0)
+#hist = history.history # Interesting if there are several epochs
 
-## ROC analysis
+# These are the predictions the model makes of the validation images (as per
+# the exercise)
+val = model.predict(val_gen)
 
-# The code has run and has been saved in the GitHub and can be retrieved:
+# Now the actual class of the val_gen data is needed:
+labels = val_gen.labels#class_indices#.keys()
+
+fpr, tpr, thresholds = roc_curve(labels,val)
+plt.plot(fpr,tpr)
 
 
 
-# TODO Perform ROC analysis on the validation set
 
 
 
-## Exercise 2
 
-def get_conv_model(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filters=64):
 
-     # build the model
-     model = Sequential()
 
-     model.add(Conv2D(first_filters, kernel_size, activation = 'relu', padding = 'same', input_shape = (IMAGE_SIZE, IMAGE_SIZE, 3)))
-     model.add(MaxPool2D(pool_size = pool_size))
 
-     model.add(Conv2D(second_filters, kernel_size, activation = 'relu', padding = 'same'))
-     model.add(MaxPool2D(pool_size = pool_size))
+
+
+
+
+
+
+
+# ## ROC analysis
+
+# # The code has run and has been saved in the GitHub and can be retrieved:
+
+
+
+# # TODO Perform ROC analysis on the validation set
+
+
+
+# ## Exercise 2
+
+# def get_conv_model(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filters=64):
+
+#      # build the model
+#      model = Sequential()
+
+#      model.add(Conv2D(first_filters, kernel_size, activation = 'relu', padding = 'same', input_shape = (IMAGE_SIZE, IMAGE_SIZE, 3)))
+#      model.add(MaxPool2D(pool_size = pool_size))
+
+#      model.add(Conv2D(second_filters, kernel_size, activation = 'relu', padding = 'same'))
+#      model.add(MaxPool2D(pool_size = pool_size))
      
-     model.add(Conv2D(64, kernel_size, activation = 'relu', padding = 'same')) # this was calculated to give the same parameter number (in total)
+#      model.add(Conv2D(64, kernel_size, activation = 'relu', padding = 'same')) # this was calculated to give the same parameter number (in total)
      
      
-     model.add(Flatten())
-     model.add(Dense(1, activation = 'sigmoid'))
+#      model.add(Flatten())
+#      model.add(Dense(1, activation = 'sigmoid'))
 
 
-     # compile the model
-     model.compile(SGD(learning_rate=0.01, momentum=0.95), loss = 'binary_crossentropy', metrics=['accuracy'])
+#      # compile the model
+#      model.compile(SGD(learning_rate=0.01, momentum=0.95), loss = 'binary_crossentropy', metrics=['accuracy'])
      
-     model.summary()
+#      model.summary()
 
-     return model
+#      return model
 
-# get the model
-conv_model = get_conv_model()
-
-
-# save the model and weights
-conv_model_name = 'my_conv_cnn_model'
-conv_model_filepath = model_name + '.json'
-conv_weights_filepath = conv_model_name + '_weights.hdf5'
-
-conv_model_json = conv_model.to_json() # serialize model to JSON
-with open(conv_model_filepath, 'w') as json_file:
-    json_file.write(conv_model_json)
+# # get the model
+# conv_model = get_conv_model()
 
 
-# define the model checkpoint and Tensorboard callbacks
-conv_checkpoint = ModelCheckpoint(conv_weights_filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-conv_tensorboard = TensorBoard(os.path.join('logs', model_name))
-conv_callbacks_list = [conv_checkpoint, conv_tensorboard]
+# # save the model and weights
+# conv_model_name = 'my_conv_cnn_model'
+# conv_model_filepath = model_name + '.json'
+# conv_weights_filepath = conv_model_name + '_weights.hdf5'
+
+# conv_model_json = conv_model.to_json() # serialize model to JSON
+# with open(conv_model_filepath, 'w') as json_file:
+#     json_file.write(conv_model_json)
 
 
-# train the model
+# # define the model checkpoint and Tensorboard callbacks
+# conv_checkpoint = ModelCheckpoint(conv_weights_filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+# conv_tensorboard = TensorBoard(os.path.join('logs', model_name))
+# conv_callbacks_list = [conv_checkpoint, conv_tensorboard]
 
 
-conv_history = model.fit(train_gen, steps_per_epoch=train_steps,
-                    validation_data=val_gen,
-                    validation_steps=val_steps,
-                    epochs=3,
-                    callbacks=conv_callbacks_list)
+# # train the model
 
 
-# compare the two models
+# conv_history = model.fit(train_gen, steps_per_epoch=train_steps,
+#                     validation_data=val_gen,
+#                     validation_steps=val_steps,
+#                     epochs=3,
+#                     callbacks=conv_callbacks_list)
 
-conv_score = conv_history.evaluate(val_gen, val_gen_keys, verbose=0)
 
-print('Test loss original model:', score[0])
-print('Test loss fully convolutional model:', conv_score[0])
-print('Test accuracy original model:', score[1])
-print('Test accuracy fully convolutional model:', conv_score[1])
+# # compare the two models
+
+# conv_score = conv_history.evaluate(val_gen, val_gen_keys, verbose=0)
+
+# print('Test loss original model:', score[0])
+# print('Test loss fully convolutional model:', conv_score[0])
+# print('Test accuracy original model:', score[1])
+# print('Test accuracy fully convolutional model:', conv_score[1])
